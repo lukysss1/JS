b25c80e55730ae29ab808d8fa45e2cf2a36c0a88
LordLukys <lukaskovarik0102@gmail.com>
2026-02-04 11:39:32 +0100

    update for the niggas

---
+++ 3.3.2/script.js
@@ -0,0 +1,21 @@
+let userName = "Tomáš"; 
+console.log(userName);
+
+userName = "Natalia";
+console.log(userName);
+
+console.log(5 + 5);
+console.log("7" + "3");
+console.log("A" + "B");
+console.log(20 / 4);
+
+let age = 30;
+console.log(age>20);
+
+let text = "Hello";
+console.log(typeof text);
+
+let a;
+console.log(a);
+let b = null;
+console.log(b);
